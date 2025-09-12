ccc506e34378f18f2deaa7b79fbc5d96caa3958c
Ruhnehral Mikail <ruhnehral@Ruhnehrals-MacBook-Pro.local>
2025-09-12 15:16:24 +0700

    penambahan migration dan model employee

---
+++ app/Models/Employee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_lengkap',
+        'email',
+        'nomor_telepon',
+        'tanggal_lahir',
+        'alamat',
+        'tanggal_masuk',
+        'status',
+    ];
+}
