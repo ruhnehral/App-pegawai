f4d401f186e2ca21d7da29296f14e1a3245cdddf
Ruhnehral Mikail <ruhnehral@Ruhnehrals-MacBook-Pro.local>
2025-10-23 21:43:02 +0700

    Penambahan migrasi semua tabel

--- database/migrations/2025_09_12_081351_create_employees_table.php
+++ database/migrations/2025_09_12_081351_create_employees_table.php
@@ -6,27 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('nama_lengkap', 100);
-            $table->string('email', 100);
-            $table->string('nomor_telepon', 15);
-            $table->date('tanggal_lahir');
-            $table->text('alamat');
+            $table->string('email', 100)->unique();
+            $table->string('no_hp', 15)->nullable();
             $table->date('tanggal_masuk');
-            $table->enum('status', ['aktif', 'nonaktif'])->default('aktif');
+            $table->unsignedBigInteger('departemen_id');
+            $table->unsignedBigInteger('jabatan_id');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('employees');
---
+++ database/migrations/2025_10_22_110505_add_department_position_to_employees.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::table('employees', function (Blueprint $table) {
+        $table->unsignedBigInteger('departemen_id')->after('tanggal_masuk')->nullable();
+        $table->unsignedBigInteger('jabatan_id')->after('departemen_id')->nullable();
+
+        $table->foreign('departemen_id')
+              ->references('id')->on('departments')
+              ->onDelete('cascade');
+
+        $table->foreign('jabatan_id')
+              ->references('id')->on('positions')
+              ->onDelete('cascade');
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            if (Schema::hasColumn('employees', 'departemen_id')) {
+                $table->dropForeign(['departemen_id']);
+            }
+            if (Schema::hasColumn('employees', 'jabatan_id')) {
+                $table->dropForeign(['jabatan_id']);
+            }
+            if (Schema::hasColumn('employees', 'departemen_id') && Schema::hasColumn('employees', 'jabatan_id')) {
+                $table->dropColumn(['departemen_id', 'jabatan_id']);
+            }
+        });
+    }
+};
